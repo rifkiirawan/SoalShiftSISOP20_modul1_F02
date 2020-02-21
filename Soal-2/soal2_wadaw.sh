b9d2131fbcab2e381670442d0180deb9115089c0
rifkiirawan <57948738+rifkiirawan@users.noreply.github.com>
2020-02-21 16:29:49 +0700

    Update soal2_wadaw.sh

--- Soal-2/soal2_wadaw.sh
+++ Soal-2/soal2_wadaw.sh
@@ -2,18 +2,12 @@
 
 after="${1%.*}"
 
-lasttime=$(date +%H -r $1)
+jam=$(date +%H -r $1)
 
-atas=`expr $lasttime + 98`
-bawah=`expr $lasttime + 97`
-
-chr() {
-  printf "\\$(printf '%03o' "$1")"
-}
-
-upper=`chr $atas`
-lower=`chr $bawah`
-
-before=`printf "$after" | tr $upper-za-$lower${upper^^}-ZA-${lower^^} b-zaB-ZA`
+while [ $jam -gt 0 ]
+do
+ before=$(echo $after | tr '[B-ZAb-za]' '[A-Za-z]')
+ jam=$((jam-1))
+done
 
 mv $after.txt $before.txt
