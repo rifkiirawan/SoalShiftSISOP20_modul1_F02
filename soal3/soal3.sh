b4bfc9f09cd8f090e1bfc18c38af4384fa133bb5
Evelyn Tjitrodjojo <53274827+marsellaeve@users.noreply.github.com>
2020-02-26 21:36:02 +0700

    Update soal3.sh

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-for i in {1..28}
-do
-	wget "https://loremflickr.com/320/240/cat" -O "/home/evelyn/Praktikum1/pdkt_kusuma_$i" -a "/home/evelyn/Praktikum1/wget.log"
-done
 
-5 6-23/8 * * 1-5,7 /bin/bash /home/evelyn/Praktikum1/soal3a.sh
+grep "Location" wget.log > location.log
+readarray myarray < location.log
 
 for i in {1..28}
 do
@@ -13,7 +10,7 @@ do
 		if [ $i -eq $j ]
 		then
 			continue
-		elif cmp -s "pdkt_kusuma_$i" "pdkt_kusuma_$j"
+		elif [ "${myarray[$i]}" = "${myarray[$j]}" ]
 		then
 			mv pdkt_kusuma_$i ./duplicate/duplicate_$i
 		fi
